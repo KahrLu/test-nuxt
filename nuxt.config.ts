95edaf77eb182534e2422d009300d90b0812c7a5
Kahr Lu <kahr@planetart.com>
2022-08-19 12:31:03 +0800

    remove

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,19 +2,5 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  axios: {
-		//baseUrl: 'https://localhost:5001',
-		proxy: true
-	},
-	proxy: {
-		'/api': {
-			changeOrigin: true,
-			target: 'https://cafepresswebapi-stage.cafepress.com',
-			pathRewrite: {
-				'^/api': '/api'
-			},
-			secure: false
-		}
-	},
-	modules: ['@nuxtjs-alt/axios', '@nuxtjs-alt/proxy']
+
 })
